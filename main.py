6c3b4e1da8bc945745cead053dfba01bb57ee29a
masi <masiunnisa.13@gmail.com>
2021-08-19 21:18:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,72 @@
+# import turtle
+# import pandas
+#
+# screen = turtle.Screen()
+# screen.title("India States Game")
+# image = "blank_states_img.gif"
+# screen.addshape(image)
+# turtle.shape(image)
+#
+# data = pandas.read_csv("states_data.csv")
+# all_states = data.state.to_list()
+# guessed_state = []
+#
+# while len(guessed_state) < 29:
+#     answer = screen.textinput(title=f"{len(guessed_state)}/29 States Correct", prompt="Guess the State name").title()
+#
+#     if answer == "Exit":
+#         missing_states = []
+#         for state in all_states:
+#             if state not in guessed_state:
+#                 missing_states.append(state)
+#         new_data = pandas.DataFrame(missing_states)
+#         new_data.to_csv("States_to_learn.csv")
+#         break
+#
+#     if answer in all_states:
+#         t = turtle.Turtle()
+#         t.hideturtle()
+#         t.penup()
+#         state_data = data[data.state == answer]
+#         t.goto(int(state_data.x), int(state_data.y))
+#         t.write(answer)
+#
+# screen.exitonclick()
+
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("India States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv("states_data.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 29:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/29 states correct",
+                                    prompt="What's another state's name ?").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
